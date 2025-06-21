7093810eed81ff158de134245960fff8ee0ee035
fathan09 <totobagindang@gmail.com>
2025-06-21 13:20:19 +0800

    Add files via upload
    changes on component

--- nt-event/src/components/TicketCart.vue
+++ nt-event/src/components/TicketCart.vue
@@ -4,63 +4,67 @@
         <div class="box-cart">
             <h2> Items In Cart </h2>
 
-            <div class="cart-item"> 
-                <p>3</p>
+            <div v-for="(ticket, index) in ticketItems" :key="index" class="cart-item"> 
+                <p>{{ticket.quantity}}</p>
                 <div class="ticket-info">
-                    <p>Ticket Type 1</p>
-                    <p>21st May 2025</p>
+                    <p>{{ticket.ticketName}}</p>
+                    <p>{{ticket.deadline}}</p>
+                    <button class="reduce-button" @click="reduceFromCart(ticket)">
+                        Reduce
+                    </button>
+                    <input type="number" :value="getQuantity(ticket)" readonly />
+                    <button class="add-button" @click="addToCart(ticket)">
+                        Add
+                    </button>
                 </div>
                 <div class="unit-price">
-                    <p>RM 150</p>
+                    <p>RM {{ticket.quantity * ticket.price}}</p>
                 </div>   
             </div>
-
-            <hr style="border: 0; border-top: 1px solid #ccc; margin: 10px 0;" />
- 
-
-            <div class="cart-item"> 
-                <p>2</p>
-                <div class="ticket-info">
-                    <p>Ticket Type 2</p>
-                    <p>21st May 2025</p>
-                </div>
-                <div class="unit-price">
-                    <p>RM 60</p>
-                </div>   
-            </div>
-
-            <hr style="border: 0; border-top: 1px solid #ccc; margin: 10px 0;" />
-
-
-            <div class="cart-item"> 
-                <p>1</p>
-                <div class="ticket-info">
-                    <p>Ticket Type 3</p>
-                    <p>21st May 2025</p>
-                </div>
-                <div class="unit-price">
-                    <p>RM 10</p>
-                </div>               
-            </div>
-
-            <hr style="border: 0; border-top: 1px solid #ccc; margin: 10px 0;" />
 
 
             <div class="total-price">
-                <p>Total Price RM 220</p>
+                <p>Total Price RM {{totalPrice}}</p>
             </div>
         </div>
         <div class="button-container">
             <button id="button-cancel"> <router-link to="/" class="router"> Cancel </router-link> </button>
-            <button id="button-buy"> Checkout Items </button>
+            <button id="button-buy"> <router-link to="/merch" class="router"> Buy Tickets </router-link> </button> 
         </div>
         
     </div>
 </template>
 
 <script>
+import {useTicketState} from "../store/ticketStore.js";
 
 export default {
-    name: "TicketCart"
+    name: "TicketCart",
+    computed: {
+        ticketItems() {
+            return this.ticketStore.ticketItems;
+        },
+        totalPrice() {
+            return this.ticketStore.totalPrice;
+        } 
+    },
+    setup() {
+        const ticketStore = useTicketState();
+        return {ticketStore};
+    },
+    methods: {
+        addToCart(ticket) {
+            this.ticketStore.addToCart(ticket);
+        },
+        reduceFromCart(ticket) {
+            this.ticketStore.reduceFromCart(ticket);
+        },
+        getQuantity(ticket) {
+            const item = this.ticketStore.ticketItems.find(
+                (t) => t.ticketName === ticket.ticketName
+            );
+            return item ? item.quantity : 0;
+        }
+    },
 }
 </script>
